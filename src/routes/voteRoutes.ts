af13739e000e160e6bff8d6e4614948f7fa6fb98
righteousness <righteousness@righteousness-MacBook-Air.local>
2026-02-19 12:52:08 +0100

    allow all users to view results

--- src/routes/voteRoutes.ts
+++ src/routes/voteRoutes.ts
@@ -8,7 +8,7 @@ import { castVoteSchema } from "../validators/voteValidators";
 const router = Router();
 
 router.post("/", protect, voteLimiter, validate(castVoteSchema), castVote);
-router.get("/:electionId/results", protect, isAdmin, getResults);
+router.get("/:electionId/results", protect, getResults);
 router.get("/:electionId/verify", protect, isAdmin, verifyChain);
 
 export default router;
